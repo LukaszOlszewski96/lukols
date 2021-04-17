c2ac8cbcb5af4610010ba78aa052bca6fd6420c4
LukaszOlszewski96 <lukaszolszewski96@gmail.com>
2021-04-17 15:03:18 +0200

    Contact form before add graphics: 80%

--- src/Page/Contact/ContactForm/ContactForm.js
+++ src/Page/Contact/ContactForm/ContactForm.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import "./ContactForm.css";
-import {AiFillCloseSquare} from "react-icons/ai";
+import {IoPersonCircleOutline} from "react-icons/io5";
+import {HiOutlineMail} from "react-icons/hi";
+import {MdTitle} from "react-icons/md";
 
 function ContactForm() {
     return (
@@ -11,7 +13,25 @@ function ContactForm() {
                     <p className="title-contact-form">Get in touch</p>
                     <p className="description-contact-form">Feel free to get in touch with me. I am always open to discussing new projects, creative ideas or opportunities to be part of your visions.</p>
                 </div>
-                <div className="input-box-contact"></div>
+                <img alt="contact" src="image/contact_image.png"/>
+                <div className="input-box-contact">
+                    <form className="name-input-form">
+                        <div className="icon"><IoPersonCircleOutline/></div>
+                        <input className="name-input" type="text" placeholder="Your name..."/>
+                    </form>
+                    <form className="name-input-form">
+                        <div className="icon"><HiOutlineMail/></div>
+                        <input className="name-input" type="text" placeholder="Your email..."/>
+                    </form>
+                    <form className="topic-input-form">
+                        <div className="icon"><MdTitle/></div>
+                        <input className="topic-input" type="text" placeholder="Topic..."/>
+                    </form>
+                    <form className="textarea-input-form">
+                        <textarea className="text-area-input" type="text" />
+                    </form>
+                    <button type="submit" className="button-submit">Submit</button>
+                </div>
                     </div>
                 </div>
             </div>
